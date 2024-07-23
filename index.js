f78a0847a440c0e52c283fb3583c678cf946448f
Risvi-Tareq <ivsirqerat@gmail.com>
2024-07-23 15:59:28 -0700

    mouse events work and can trigger ound

--- index.js
+++
@@ -1,7 +0,0 @@
-// The encrypted path as a JSON array of ASCII values
-const encryptedPathAscii = [50,51,49,56,48,97,56,51,55,57,57,56,54,53,101,57,57,100,52,52,48,50,52,55,50,102,54,100,97,99,98]
-
-// Convert the ASCII values to characters and join them into a string
-const encryptedPath = encryptedPathAscii.map(code => String.fromCharCode(code)).join('');
-
-console.log(`Decrypted path: ${encryptedPath}`);
